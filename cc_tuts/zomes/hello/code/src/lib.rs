3a82dc1487b7528fe0c6fc940f3dfb4709dacac5
freesig <tomrgowan@gmail.com>
2020-01-16 11:25:19 +1100

    initial commit

---
+++ cc_tuts/zomes/hello/code/src/lib.rs
@@ -0,0 +1,115 @@
+#![feature(proc_macro_hygiene)]
+#[macro_use]
+extern crate hdk;
+extern crate hdk_proc_macros;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+#[macro_use]
+extern crate holochain_json_derive;
+use hdk::{
+    entry_definition::ValidatingEntryType,
+    error::ZomeApiResult,
+};
+
+use hdk::holochain_core_types::{
+    entry::Entry,
+    dna::entry_types::Sharing,
+    link::LinkMatch,
+};
+
+use hdk::holochain_json_api::{
+    json::JsonString,
+    error::JsonError,
+};
+
+  use hdk::holochain_persistence_api::{
+    cas::content::Address
+};
+
+use hdk_proc_macros::zome;
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct Post {
+    message: String,
+    timestamp: u64,
+    author_id: Address,
+}
+#[zome]
+mod hello_zome {
+    #[init]
+    fn init() {
+        Ok(())
+    }
+    #[validate_agent]
+    pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
+        Ok(())
+    }
+    
+    #[zome_fn("hc_public")]
+    pub fn hello_holo() -> ZomeApiResult<String> {
+        Ok("Hello Holo".into())
+    }
+    #[entry_def]
+    fn post_entry_def() -> ValidatingEntryType {
+        entry!(
+            name: "post",
+            description: "A blog post",
+            sharing: Sharing::Public,
+            validation_package: || {
+                hdk::ValidationPackageDefinition::Entry
+            },
+            validation: | validation_data: hdk::EntryValidationData<Post>| {
+                match validation_data {
+                    hdk::EntryValidationData::Create{ entry, .. } => {
+                        const MAX_LENGTH: usize = 140;
+                        if entry.message.len() <= MAX_LENGTH {
+                           Ok(())
+                        } else {
+                           Err("Post too long".into())
+                        }
+                    },
+                    _ => Ok(()),
+                }
+            },
+            links: [
+                from!(
+                   "%agent_id",
+                   link_type: "author_post",
+                   validation_package: || {
+                       hdk::ValidationPackageDefinition::Entry
+                   },
+                   validation: |_validation_data: hdk::LinkValidationData| {
+                       Ok(())
+                   }
+                )
+            ]
+        )
+    }
+    #[zome_fn("hc_public")]
+    pub fn create_post(message: String, timestamp: u64) -> ZomeApiResult<Address> {
+        let post = Post {
+            message,
+            timestamp,
+            author_id: hdk::AGENT_ADDRESS.clone(),
+        };
+        let agent_address = hdk::AGENT_ADDRESS.clone().into();
+        let entry = Entry::App("post".into(), post.into());
+        let address = hdk::commit_entry(&entry)?;
+        hdk::link_entries(&agent_address, &address, "author_post", "")?;
+        
+        Ok(address)
+    }
+    #[zome_fn("hc_public")]
+    pub fn retrieve_posts(agent_address: Address) -> ZomeApiResult<Vec<Post>> {
+        hdk::utils::get_links_and_load_type(
+            &agent_address,
+            LinkMatch::Exactly("author_post"),
+            LinkMatch::Any,
+        )
+    }
+    #[zome_fn("hc_public")]
+    pub fn get_agent_id() -> ZomeApiResult<Address> {
+        Ok(hdk::AGENT_ADDRESS.clone())
+    }
+}
