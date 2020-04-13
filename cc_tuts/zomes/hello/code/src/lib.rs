e49686223c7ed424a533e0482b4345e6c9286e7d
freesig <tomrgowan@gmail.com>
2020-04-13 14:10:06 +1000

    update

--- cc_tuts/zomes/hello/code/src/lib.rs
+++ cc_tuts/zomes/hello/code/src/lib.rs
@@ -1,26 +1,16 @@
 #![feature(proc_macro_hygiene)]
-extern crate hdk;
-extern crate hdk_proc_macros;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-extern crate holochain_json_derive;
 
-use hdk::{
-    error::ZomeApiResult,
-};
-
+use hdk::prelude::*;
 use hdk_proc_macros::zome;
 
-// see https://developer.holochain.org/api/0.0.40-alpha1/hdk/ for info on using the hdk library
+// see https://developer.holochain.org/api/0.0.47-alpha1/hdk/ for info on using the hdk library
 
 // This is a sample zome that defines an entry type "MyEntry" that can be committed to the
 // agent's chain via the exposed function create_my_entry
 
 
 #[zome]
- mod hello_zome {
+mod hello_zome {
 
     #[init]
     fn init() {
@@ -31,7 +21,6 @@ use hdk_proc_macros::zome;
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
         Ok(())
     }
-
 
     #[zome_fn("hc_public")]
     pub fn hello_holo() -> ZomeApiResult<String> {
